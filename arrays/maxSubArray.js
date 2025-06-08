be9687f1d870ccd6c0b9db0213be97de49fc7dc1
venkatthondamalla98 <venkatthondamalla98@gmail.com>
2025-06-08 23:36:08 +0530

    feat: :sparkles: find the subarray with the largest sum, and return its sum
    find the subarray with the largest sum, and return its sum

---
+++ arrays/maxSubArray.js
@@ -0,0 +1,30 @@
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+// Example 1:
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+// Example 2:
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+
+// Example 3:
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+
+function findSubArray(array){
+    let maxSum = array[0];
+    let currentSum = array[0]
+    for(let i=1; i<array.length; i++){
+        currentSum = Math.max(array[i], currentSum + array[i])
+        maxSum = Math.max(currentSum, maxSum)
+    }
+    return maxSum
+}
+
+
+console.log(findSubArray([5,4,-1,7,8]))
